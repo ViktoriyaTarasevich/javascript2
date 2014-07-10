b7918e61015df0781339e6457e9fe1ef95400965
ViktoriyaTarasevich <viktoriya.v.tarasevich@gmail.com>
2014-07-11 00:33:10 +0300

    [Update]: add memoization, first, curry, filter, getAverangeEvenNumbers, lazye evaluation.

--- functions.js
+++ functions.js
@@ -6,6 +6,21 @@ function partialApplication(f) {
     };
 };
 
+function curry(f, inner_args){
+  var args = Array.prototype.slice.call(arguments,1);
+  var n = 0;
+  return (function curriedFunction(n){
+    return function(){
+      for(var i=0; i<arguments.length; i++) args[i + n] = arguments[i];
+		var newArgumentsLength = n + arguments.length;
+      if(newArgumentsLength < f.length) 
+		return curriedFunction(newArgumentsLength);
+	   args.length = newArgumentsLength;
+      return f.apply(inner_args, args);
+    };
+  })(n);
+}
+
 function linearHolder(array, callback,initialValue){
 	var previousValue = initialValue;
 	var callbackValue;
@@ -26,7 +41,7 @@ function linearUnholder(callback,initialValue){
 	return arrayResult;
 }
 
-function Map(array, func){
+function map(func, array){
 	var newArray = [];
 	for (var i = 0; i < array.length; i++){
 		newArray[i] = func(array[i]);
@@ -34,8 +49,48 @@ function Map(array, func){
 	return newArray;
 }
 
-function Filter(){
+function filter(func,array){
+	var newArray = [];
+	for(var i = 0; i< array.length; i++){
+		if(func(array[i])){
+			console.log(array[i]);
+			newArray.push(array[i]);
+		}
+	}
+	return newArray;
+}
+
+function getAverangeEvenNumbers(array){
+	var arr = filter(function(x){if(x%2===0) return x;},array);
+	return arr;
+}
+
+function first(array, condition){
+	for (var i = 0; i < array.length; i++){
+		if (condition(array[i]){
+			return array[i];
+	}
+}
+
+function lazyEvaluation(func){
+	lazyEvaluation = function () {
+	  return variable  = Array.prototype.slice.call(arguments);
+	};
+	return func.apply(this, arguments);
 
 }
 
-
+function memoization(func){
+	var cache = {};
+	if (arguments[0])
+		var arg = arguments[0];
+	return function(arg){
+		var args = Array.prototype.slice.call(arguments);
+		if (arg in cache){
+			console.log(cache[arg]);
+		}
+		else{
+			console.log(cache[arg] =  func.apply(this,args );
+		}
+	}
+}
